5b8aae826abd4c86395dc3590dc031fc95fc9ae9
Dobry <84011609+szmdm@users.noreply.github.com>
2022-10-06 21:42:04 +0200

    react api practice

--- 220927/index.js
+++ 220927/index.js
@@ -1,5 +1,18 @@
 const express = require('express')
 const app = express()
 
+
 app.listen(3000, () => console.log('listening at 3000'))
 app.use(express.static('public'))
+app.use(express.json({ limit: '1mb'}))
+
+app.post("/api", (request, response) => {
+    console.log(request)
+    const data = request.body
+    response.json({
+        status: 'success',
+        latitude: data.lat,
+        longitude: data.lon,
+        timestamp: data.time
+    })
+})
