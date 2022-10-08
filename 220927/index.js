f0343bb772c9043e4bfea0e04d712de81b827988
Dobry <84011609+szmdm@users.noreply.github.com>
2022-10-08 22:42:56 +0200

    got data on client fron db

--- 220927/index.js
+++ 220927/index.js
@@ -11,18 +11,21 @@ app.use(express.json({ limit: '1mb' }))
 const database = new Datastore('database.db')
 database.loadDatabase()
 
+app.get('/api', (request, response) => {
+    database.find({}, (err, data) => {
+        if (err) {
+            response.end()
+            return
+        }
+        response.json(data)
+    })
+})
+
 app.post("/api", (request, response) => {
     console.log('got request')
     const data = request.body
     const timestamp = Date.now()
     data.timestamp = timestamp  
     database.insert(data)
-    response.json({
-        status: 'success',
-        timestamp: timestamp,
-        latitude: data.lat,
-        longitude: data.lon,
-        favourite: data.vege,
-    }
-    )
+    response.json({data})
 })
